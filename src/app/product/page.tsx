4b51fdd1d7f90d632ab5cf28778846675c11218c
asmajawaid <arshadasma45678@gmail.com>
2025-02-02 02:41:03 +0500

    error handel

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -144,69 +144,69 @@ const ProductCard = ({ product }: { product: (typeof products)[0] }) => (
   </Link>
 );
 
-// export default function ProductPage() {
-//   return (
-//     <div className="max-w-[1320px] mx-auto p-8">
-//       <h1 className="text-[#272343] text-[32px] leading-[35px] text-start mb-8">
-//         All Products
-//       </h1>
+export default function ProductPage() {
+  return (
+    <div className="max-w-[1320px] mx-auto p-8">
+      <h1 className="text-[#272343] text-[32px] leading-[35px] text-start mb-8">
+        All Products
+      </h1>
     
-//       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 p-3">
-//         {products.concat(products).map((product) => (
-//           <ProductCard key={product.id} product={product} />
-//         ))}
-//       </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 p-3">
+        {products.concat(products).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
       
 
-//       {/* form for email input */}
-//       <div className="mt-16 bg-[#F0F2F3] w-full">
-//         <div className="flex flex-col justify-center items-center p-5">
-//           <h2 className="text-[24px] md:text-[30px] font-medium leading-[40px] md:leading-[58px] text-center">
-//             Or subscribe to the newsletter
-//           </h2>
-//           <form action="" className="flex flex-col sm:flex-row gap-4 mt-5">
-//             <input
-//               type="email"
-//               placeholder="Email Address..."
-//               className="w-full sm:w-[400px] md:w-[600px] h-[40px] border hover:border-b-4 border-gray-400 p-2"
-//             />
-//             <button className="w-full sm:w-auto h-[40px] px-4 text-center cursor-pointer border text-black hover:border-b-4 border-gray-400 transition-colors">
-//               SUBMIT
-//             </button>
-//           </form>
-//         </div>
-//       </div>
-//       {/* multi image components */}
-//       <div className="flex flex-col justify-center items-center p-5">
-//         <h2 className="text-[30px] leading-[58px] font-medium">
-//           Follow products and discounts on Instagram
-//         </h2>
-//         <div className="p-3"> {/* <ImageRow />{" "} */}</div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-async function getProductById(id: string) {
-  return products.find((product) => product.id === id) || null;
-}
-
-export default async function ProductPage({ params }: ProductDetailProps) {
-  const product = await getProductById(params.id);
-
-  if (!product) {
-    return notFound();
-  }
-
-  return (
-    <div className="max-w-4xl mx-auto p-8">
-      <h1 className="text-2xl font-semibold">{product.title}</h1>
-      <Image src={product.image} alt={product.title} width={400} height={400} />
-      <p className="mt-4 text-xl">Price: {product.price}</p>
+      {/* form for email input */}
+      <div className="mt-16 bg-[#F0F2F3] w-full">
+        <div className="flex flex-col justify-center items-center p-5">
+          <h2 className="text-[24px] md:text-[30px] font-medium leading-[40px] md:leading-[58px] text-center">
+            Or subscribe to the newsletter
+          </h2>
+          <form action="" className="flex flex-col sm:flex-row gap-4 mt-5">
+            <input
+              type="email"
+              placeholder="Email Address..."
+              className="w-full sm:w-[400px] md:w-[600px] h-[40px] border hover:border-b-4 border-gray-400 p-2"
+            />
+            <button className="w-full sm:w-auto h-[40px] px-4 text-center cursor-pointer border text-black hover:border-b-4 border-gray-400 transition-colors">
+              SUBMIT
+            </button>
+          </form>
+        </div>
+      </div>
+      {/* multi image components */}
+      <div className="flex flex-col justify-center items-center p-5">
+        <h2 className="text-[30px] leading-[58px] font-medium">
+          Follow products and discounts on Instagram
+        </h2>
+        <div className="p-3"> {/* <ImageRow />{" "} */}</div>
+      </div>
     </div>
   );
 }
+
+
+// async function getProductById(id: string) {
+//   return products.find((product) => product.id === id) || null;
+// }
+
+// export default async function ProductPage({ params }: ProductDetailProps) {
+//   const product = await getProductById(params.id);
+
+//   if (!product) {
+//     return notFound();
+//   }
+
+//   return (
+//     <div className="max-w-4xl mx-auto p-8">
+//       <h1 className="text-2xl font-semibold">{product.title}</h1>
+//       <Image src={product.image} alt={product.title} width={400} height={400} />
+//       <p className="mt-4 text-xl">Price: {product.price}</p>
+//     </div>
+//   );
+// }
 
 {/* <Link href={`/product/${product.id}`}>
   <ProductCard product={product} />
